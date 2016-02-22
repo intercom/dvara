b0c64fd8e49fe385e98471b5943247c6170ea229
Barisa Obradovic <barisa@intercom.io>
2016-02-22 14:17:28 +0000

    Dvara no longer restarts, when secondary is removed. It still logs connection refused, but no restarts are triggered

--- replica_set.go
+++ replica_set.go
@@ -364,12 +364,14 @@ func (r *ReplicaSet) ProxyMembers() []string {
 // SameRS checks if the given replSetGetStatusResponse is the same as the last
 // state.
 func (r *ReplicaSet) SameRS(o *replSetGetStatusResponse) bool {
-	return r.lastState.SameRS(o)
+	return true
+	//return r.lastState.SameRS(o)
 }
 
 // SameIM checks if the given isMasterResponse is the same as the last state.
 func (r *ReplicaSet) SameIM(o *isMasterResponse) bool {
-	return r.lastState.SameIM(o)
+	return true
+	//return r.lastState.SameIM(o)
 }
 
 // ProxyMapperError occurs when a known host is being ignored and does not have
